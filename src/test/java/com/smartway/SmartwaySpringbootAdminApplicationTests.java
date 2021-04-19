d1698e5db6d0f89963ea57eafdbd61f556d761e1
Surendra babu Boyilla <surendrababu.boyilla@pccw.com>
2021-04-19 19:56:04 +0800

    First commit Smartway Spring Boot admin app

---
+++ src/test/java/com/smartway/SmartwaySpringbootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartwaySpringbootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
